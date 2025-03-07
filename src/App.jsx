a0cbca7c6c3b773c3ce048c1960266301e3a8ad5
max <hmwright404@gmail.com>
2025-03-07 10:05:25 -0600

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import Board from './components/Board'
+
+function App() {
+
+  return (
+    <>
+      <div className="container">
+        <Board />
+      </div>
+    </>
+  )
+}
+
+export default App
